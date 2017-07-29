002e4d1f03b6401e2ca33849eee60549a7a9ddd0
venkat116 <venkat116@users.noreply.github.com>
2017-07-29 07:36:47 +0530

    Update employee_chaincode.go

--- employee_chaincode.go
+++ employee_chaincode.go
@@ -90,7 +90,7 @@ type User_and_eCert struct {
 //==============================================================================================================================
 //	Init Function - Called when the user deploys the chaincode
 //==============================================================================================================================
-/*func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
 	//Args
@@ -110,8 +110,8 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 
 	return nil, nil
-}*/
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+}
+/*func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
@@ -124,7 +124,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		t.add_ecert(stub, args[i], args[i+1])
 	}
 	return nil, nil
-}
+}*/
 
 
 //==============================================================================================================================
@@ -256,7 +256,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 
 
 	if function == "create_vehicle" {
-        return t.create_vehicle(stub, caller, caller_affiliation, args[0])
+        return t.create_vehicle(stub, caller, caller_affiliation, args)
 	} else if function == "addemp" {
 		return t.addEmp(stub,caller, caller_affiliation, args)
 	} else if function == "ping" {
@@ -341,10 +341,10 @@ func (t *SimpleChaincode) ping(stub shim.ChaincodeStubInterface) ([]byte, error)
 //=================================================================================================================================
 //	 Create Vehicle - Creates the initial JSON for the vehcile and then saves it to the ledger.
 //=================================================================================================================================
-func (t *SimpleChaincode) create_vehicle(stub shim.ChaincodeStubInterface, caller string, caller_affiliation string, v5cID string) ([]byte, error) {
+func (t *SimpleChaincode) create_vehicle(stub shim.ChaincodeStubInterface, caller string, caller_affiliation string, v5cID []string) ([]byte, error) {
 	var v Vehicle
 
-	v5c_ID         := "\"v5cID\":\""+v5cID+"\", "							// Variables to define the JSON
+	v5c_ID         := "\"v5cID\":\""+v5cID[0]+"\", "							// Variables to define the JSON
 	vin            := "\"VIN\":0, "
 	make           := "\"Make\":\"UNDEFINED\", "
 	model          := "\"Model\":\"UNDEFINED\", "
@@ -357,7 +357,7 @@ func (t *SimpleChaincode) create_vehicle(stub shim.ChaincodeStubInterface, calle
 
 	vehicle_json := "{"+v5c_ID+vin+make+model+reg+owner+colour+leaseContract+status+scrapped+"}" 	// Concatenates the variables to create the total JSON object
 
-	matched, err := regexp.Match("^[A-z][A-z][0-9]{7}", []byte(v5cID))  				// matched = true if the v5cID passed fits format of two letters followed by seven digits
+	matched, err := regexp.Match("^[A-z][A-z][0-9]{7}", []byte(v5cID[0]))  				// matched = true if the v5cID passed fits format of two letters followed by seven digits
 
 												if err != nil { fmt.Printf("CREATE_VEHICLE: Invalid v5cID: %s", err); return nil, errors.New("Invalid v5cID") }
 
